6c31078dd5c662290e9b3b5c27f889a3652b130e
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-11 22:35:32 +0600

    Serer user controller commit

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,124 @@
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+import dotenv from 'dotenv';
+import { createDocument, deleteDocument, findDocument } from '../database/appwrite.queries';
+dotenv.config();
+
+const encryptPassword = async(password) =>{
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    return hashedPassword;
+}
+
+const createToken = (_id) =>{
+    return jwt.sign({_id}, process.env.SECRET_WEB_KEY, {expiresIn: "10d"});
+};
+
+const registerUser = async(req, res)=>{
+
+    const {name, password} = req.body;
+
+    try {
+
+        if(!name || !password){
+            res.status(404).send({success:false, error: "All fields required"});
+        }
+    
+        else{
+    
+            const exist = await findDocument(process.env.APPWRITE_USER_COLLECTION_ID, "name", name);
+            if(exist.response.total != 0){
+                res.status(404).send({success:false, error: "Name is already in use"});
+            }
+            else{
+                
+                const hashedPassword = await encryptPassword(password);
+                const userData = {
+                    name,
+                    password: hashedPassword,
+                }
+
+                const registerResponse = await createDocument(process.env.APPWRITE_USER_COLLECTION_ID, userData);
+                if(registerResponse.success){
+                    const webToken = createToken(registerResponse.$id);
+                    res.status(201).send({success:true, name, webToken});
+                }
+                else{
+                    res.status(404).send({success:false, error: "User is not registered, please try again"});
+                }
+
+            }
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(400).send({success:false, error:e.message});
+    }
+}
+
+const loginUser = async(req, res)=>{
+
+    const {name, password} = req.body;
+
+    if(!name || !password){
+        res.status(404).send({success:false, error: "All fields are required"});
+    }
+
+    try {
+        
+        const user = await findDocument(process.env.APPWRITE_USER_COLLECTION_ID, "name", name);
+        if(user.response.total === 0){
+            res.status(404).send({success:false, error: "No such user found"});
+        }
+        else{
+            const match = await bcrypt.compare(password, user.response.documents[0].password);
+            if(!match){
+                res.status(404).send({success:false, error: "Invalid Credentials"});
+            }
+            else{
+                const webToken = createToken(user.response.documents[0].$id);
+                res.status(201).send({success:true, name, webToken});
+            }
+        }
+
+    } catch (e) {
+        console.log(e);
+        res.status(404).send({success:false, error:e.message});
+    }
+
+}
+
+const deleteUser = async(req, res)=>{
+
+    const {name} = req.body;
+
+    if(!name){
+        res.status(404).send({success:false, error: "Name is required"});
+    }
+
+    try {
+        
+        const user = await findDocument(process.env.APPWRITE_USER_COLLECTION_ID, "name", name);
+        if(user.response.total === 0){
+            res.status(404).send({success:false, error: "No such user found"});
+        }
+        else{
+            const deleteUserResponse = await deleteDocument(process.env.APPWRITE_USER_COLLECTION_ID, user.response.documents[0].$id);
+            if(deleteUserResponse.success){
+                res.status(201).send({success:true, message: "User " + name + " is deleted"});
+            }
+            else{
+                res.status(404).send({success:false, error: "User is not deleted, please try again"});
+            }
+        }
+
+    } catch (e) {
+        console.log(e);
+        res.status(404).send({success:false, error:e.message});
+    }
+
+}
+
+
+export { registerUser, loginUser, deleteUser }
